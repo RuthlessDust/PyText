69dc615c131632d3a3c5581495fc8b34267fe029
RuthlessDust <38008655+RuthlessDust@users.noreply.github.com>
2018-10-09 22:32:28 +0000

    Added label to window.

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ class MainMenu:
     def __init__(self, master):
         self.master = master
         master.title = ("PyText: Main Menu")
+        self.label = Label(master, text="Main Menu")
+        self.label.pack()
 
 root = Tk()
 menu = MainMenu(root)
