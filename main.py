a011cfaa3505c9134f5d93e69ed94f70e09dc27d
RuthlessDust <38008655+RuthlessDust@users.noreply.github.com>
2018-10-25 23:03:01 +0000

    Title and Text Editor
    Changed title of window to path_to_file and used the Text widget for editing.
    (Will push to master when I implement saving changes to the text.)

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-from tkinter import Tk, Label, Button
+import tkinter
+path_to_file = "/home/cmk/Desktop/Projects/PyText/PyText-experimental/test.txt"
+file = open(path_to_file, "r+")
+cont = file.read()
 
 class MainMenu:
     def __init__(self, master):
         self.master = master
-        master.title = ("PyText: Main Menu")
-        self.label = Label(master, text="Main Menu")
-        self.label.pack()
-
-root = Tk()
+        master.title = ("PyText: {f}".format(f=path_to_file))
+        text = tkinter.Text(master, height=29, width=60)
+        text.insert(tkinter.END, cont)
+        text.pack()
+root = tkinter.Tk()
+root.title("PyText: {f}".format(f=path_to_file))
 menu = MainMenu(root)
 root.mainloop()
